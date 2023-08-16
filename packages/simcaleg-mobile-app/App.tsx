b583bdfd7cd2c580da9907bc722bbffb743585d8
Ardian Eka Candra <ardianoption@gmail.com>
2023-08-16 12:10:42 +0700

    add simple ocr test

--- packages/simcaleg-mobile-app/App.tsx
+++ packages/simcaleg-mobile-app/App.tsx
@@ -1,41 +1,15 @@
-import { useEffect, useState } from "react";
-import DocumentScanner from "react-native-document-scanner-plugin";
-import { Button, Image, SafeAreaView } from "react-native";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Scan from "./src/Scan";
 
-export default function App() {
-  const [scannedImage, setScannedImage] = useState<any>();
+type Props = {};
 
-  const scanDocument = async () => {
-    // start the document scanner
-    const { scannedImages } = await DocumentScanner.scanDocument();
-
-    // check if undefined
-    if (scannedImages) {
-      // get back an array with scanned image file paths
-      if (scannedImages.length > 0) {
-        // set the img src, so we can view the first scanned image
-        setScannedImage(scannedImages[0]);
-      }
-    }
-  };
-
-  // useEffect(() => {
-  //   // call scanDocument on load
-  //   scanDocument();
-  // }, []);
-
+const App = (props: Props) => {
   return (
-    <>
-      <SafeAreaView>
-        {scannedImage && (
-          <Image
-            resizeMode="contain"
-            style={{ width: "100%", height: "50%" }}
-            source={{ uri: scannedImage }}
-          />
-        )}
-        <Button title="Scan KTP" onPress={scanDocument} />
-      </SafeAreaView>
-    </>
+    <SafeAreaProvider>
+      <Scan />
+    </SafeAreaProvider>
   );
-}
+};
+
+export default App;
